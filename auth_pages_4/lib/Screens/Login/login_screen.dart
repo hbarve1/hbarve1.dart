98ede2557fc6f5c4e91bcd50731ccb0fccad245b
Himank Barve <hbarve1592@gmail.com>
2022-06-15 00:34:54 +0530

    auth_pages_4 is updated

---
+++ auth_pages_4/lib/Screens/Login/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:auth_pages_4/Screens/Login/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
